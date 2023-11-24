8ae9ab956dc5a9ad2d6d38bcce3eac1a0219b411
webpluspy <webpluspy@gmail.com>
2023-11-25 00:29:54 +0500

    Make Random Password Generator & Practice hooks like useRef, UseEffect, useCaklback

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 // import Hi from './Adeel';
-import Counter from './Counter'
-import BgChanger from './Adeel2Bg'
+// import Counter from './Counter'
+// import BgChanger from './Adeel2Bg'
+// import PasswordGenerator from './Password'
+import CheckRef from './UseRef'
+import CheckEffect from './UseEffect1'
+import PasswordGenerator from './MyPassword'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +16,9 @@ root.render(
     {/* <h1> Welcome </h1> */}
     {/* <App />
     <Hi /> */}
-    <BgChanger />
+    {/* <BgChanger /> */}
+    <PasswordGenerator />
+    {/* <CheckEffect/> */}
     </>
 );
 
